22a390eb82fe0ac647ee1e28c802e8dd008823ef
skylerheininger <skyler.heininger@uvm.edu>
2024-02-26 18:23:06 -0500

    added data processing for full dataset

--- extract_features.py
+++ extract_features.py
@@ -9,31 +9,50 @@ import audiofile
 import opensmile
 
 
+if __name__ == "__main__":
+    start_time = time.time()
+    # Load csv with filenames and features into a dataframe
+    filename_df = pd.read_csv('data/filename_features.csv')
 
-file = "data/Actor_01/03-01-01-01-01-01-01.wav"
-signal, sampling_rate = audiofile.read(
-    file,
-    duration=4,
-    always_2d=True,
-)
+    # Select a target list
+    label_list = filename_df["Emotion"]
 
-smile = opensmile.Smile(
-    feature_set=opensmile.FeatureSet.eGeMAPSv02,
-    feature_level=opensmile.FeatureLevel.Functionals,
-)
-print(smile.feature_names)
+    # Initialize Smile - change features as you want
+    smile = opensmile.Smile(
+        feature_set=opensmile.FeatureSet.eGeMAPSv02,
+        feature_level=opensmile.FeatureLevel.Functionals,
+    )
 
-signals = smile.process_signal(
-    signal,
-    sampling_rate
-)
+    # Iterate and create a new dataframe
+    processed_features = []
+    for file in filename_df["Filename"]:
+        # Read in the file
+        signal, sampling_rate = audiofile.read(
+            file,
+            duration=4,
+            always_2d=True,
+        )
 
-print(signals)
+        # Process file using smile
+        signals = smile.process_signal(
+            signal,
+            sampling_rate
+        )
+        # Store to list
+        processed_features.append(signals)
 
+    # Create new dataframe to save
+    # Concatenate the processed features into a single DataFrame
+    processed_df = pd.concat([pd.DataFrame(features, columns=list(smile.feature_names)) for features in processed_features], ignore_index=True)
 
+    # Add targets
+    processed_df["target"] = label_list
 
+    # Save to csv
+    processed_df.to_csv("data/processed_features.csv")
 
-
-
+    # total time
+    end_time = time.time()
+    print(f"Elapsed time: {end_time - start_time}")
 
 
