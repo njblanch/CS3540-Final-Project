db1a7689b98ac9d5776df8ea918ad58cfce665b6
skylerheininger <skyler.heininger@uvm.edu>
2024-05-05 12:06:49 -0400

    fixed some issues

--- normalize_data.py
+++ normalize_data.py
@@ -6,16 +6,22 @@ if __name__ == "__main__":
     # Load data from csv
     data = pd.read_csv("data/processed_features.csv")
     data = data.drop(data.columns[0], axis=1)
+    features = data.iloc[:, :-2]
+    labels = data.iloc[:, -2:]
+
 
     # Normalize data
     # Initialize MinMaxScaler
     scaler = MinMaxScaler()
 
     # Fit and transform the data
-    scaled_data = scaler.fit_transform(data)
+    scaled_data = scaler.fit_transform(features)
 
     # Convert back to DataFrame
-    scaled_df = pd.DataFrame(scaled_data, columns=data.columns)
+    scaled_df = pd.DataFrame(scaled_data, columns=features.columns)
 
-    scaled_df.to_csv("data/normalized_features.csv")
+    # Join data with labels
+    joined_data = pd.concat([scaled_df, data.iloc[:, -2:]], axis=1)
+
+    joined_data.to_csv("data/normalized_features.csv")
 
